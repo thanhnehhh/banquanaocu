bb05bdb8fbed947d69d51737e91c7f9f929aafb6
Tran Ngoc Thanh <22130253@st.hcmuaf.edu.vn>
2026-06-01 01:24:10 +0700

    be Admin quan lí nguoi dung

---
+++ Backend/src/main/java/com/example/demo/service/AdminService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+
+import com.example.demo.dto.AdminUserDTO;
+import com.example.demo.dto.PageResponse;
+
+public interface AdminService {
+    PageResponse<AdminUserDTO> getAllUsers(int page, int size);
+
+    PageResponse<AdminUserDTO> searchUsers(String keyword, int page, int size);
+
+    AdminUserDTO updateUserStatus(Long maNguoiDung, Integer trangThai);
+
+    void deleteUser(Long maNguoiDung);
+
+    PageResponse<AdminUserDTO> getAllHiddenUsers(int page, int size);
+
+    PageResponse<AdminUserDTO> searchHiddenUsers(String keyword, int page, int size);
+}
